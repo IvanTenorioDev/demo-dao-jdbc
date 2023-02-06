77c11a5989b0deb94e78e535e5a58665077f78d7
IvanTenorioDev <tenoriodesenvolvedor@gmail.com>
2023-02-05 22:17:41 -0300

    findById implementation

--- src/db/DB.java
+++ src/db/DB.java
@@ -1,3 +1,4 @@
+// Este é um código Java para uma classe "DB" que representa uma conexão com um banco de dados
 package db;
 
 import java.io.FileInputStream;
@@ -10,65 +11,74 @@ import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
-
-	// Variável para armazenar a conexão com o banco de dados
+// Propriedade estática para armazenar a conexão com o banco de dados
 	private static Connection conn = null;
-	
-	// Método para obter uma conexão com o banco de dados
+
+// Método para obter uma conexão com o banco de dados
 	public static Connection getConnection() {
-		// Verifica se já existe uma conexão
+		// Verifica se a conexão já foi estabelecida
 		if (conn == null) {
 			try {
-				// Carrega as propriedades do arquivo db.properties
+				// Carrega as propriedades de conexão a partir do arquivo db.properties
 				Properties props = loadProperties();
-				
-				// Obtém a URL de conexão com o banco de dados
 				String url = props.getProperty("dburl");
-				
-				// Cria uma nova conexão com o banco de dados
+
+				// Obtém a conexão com o banco de dados
 				conn = DriverManager.getConnection(url, props);
-			}
-			catch (SQLException e) {
-				// Lança uma exceção personalizada com a mensagem de erro
+			} catch (SQLException e) {
+				// Lança uma exceção personalizada caso ocorra uma exceção de banco de dados
 				throw new DbException(e.getMessage());
 			}
 		}
+		// Retorna a conexão com o banco de dados
 		return conn;
 	}
-	
-	// Método para fechar a conexão com o banco de dados
+
+// Método para fechar a conexão com o banco de dados
 	public static void closeConnection() {
+		// Verifica se a conexão está aberta
 		if (conn != null) {
 			try {
+				// Fecha a conexão com o banco de dados
 				conn.close();
 			} catch (SQLException e) {
-				// Lança uma exceção personalizada com a mensagem de erro
+				// Lança uma exceção personalizada caso ocorra uma exceção de banco de dados
 				throw new DbException(e.getMessage());
 			}
 		}
 	}
-	
-	// Método para carregar as propriedades do arquivo db.properties
+
+// Método para carregar as propriedades de conexão a partir do arquivo db.properties
 	private static Properties loadProperties() {
 		try (FileInputStream fs = new FileInputStream("db.properties")) {
 			Properties props = new Properties();
 			props.load(fs);
 			return props;
-		}
-		catch (IOException e) {
-			// Lança uma exceção personalizada com a mensagem de erro
+		} catch (IOException e) {
+			// Lança uma exceção personalizada caso ocorra uma exceção de entrada/saída
 			throw new DbException(e.getMessage());
 		}
 	}
-	
-	// Método para fechar um objeto Statement
+
+// Método para fechar um objeto "Statement"
 	public static void closeStatement(Statement st) {
 		if (st != null) {
 			try {
 				st.close();
 			} catch (SQLException e) {
-				// Lança uma exceção personalizada com a mensagem de erro
+// Lança uma exceção personalizada "DbException" caso haja erro ao fechar o statement
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
 
+// Método para fechar um objeto "ResultSet"
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+// Lança uma exceção personalizada "DbException" caso haja erro ao fechar o result set
 				throw new DbException(e.getMessage());
 			}
 		}
