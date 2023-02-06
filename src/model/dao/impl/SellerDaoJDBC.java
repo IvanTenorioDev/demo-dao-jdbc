0faa8e5df85443e020a759ea894c5e7ea7ee5a58
IvanTenorioDev <tenoriodesenvolvedor@gmail.com>
2023-02-05 23:15:32 -0300

    Reusing instantiation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -13,73 +13,126 @@ import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao {
-	
+
 	private Connection conn;
-	
+
+//Constructor que recebe uma conexão como parâmetro e atribui ao atributo da classe
 	public SellerDaoJDBC(Connection conn) {
 		this.conn = conn;
 	}
 
+//Método para inserir vendedor
 	@Override
 	public void insert(Seller obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+//Método para atualizar vendedor
 	@Override
 	public void update(Seller obj) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+//Método para excluir vendedor por id
 	@Override
 	public void deleteById(Integer id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+//Método para encontrar vendedor por id
 	@Override
 	public Seller findById(Integer id) {
 		PreparedStatement st = null;
 		ResultSet rs = null;
 		try {
+			// Comando SQL para buscar vendedor pelo id e o departamento associado
 			st = conn.prepareStatement(
-					"SELECT seller.*,department.Name as DepName "
-					+ "FROM seller INNER JOIN department "
-					+ "ON seller.DepartmentId = department.Id "
-					+ "WHERE seller.Id = ? ");
-			
+					"SELECT seller.*,department.Name as DepName " + "FROM seller INNER JOIN department "
+							+ "ON seller.DepartmentId = department.Id " + "WHERE seller.Id = ? ");
+
+			// definindo o id para busca
 			st.setInt(1, id);
 			rs = st.executeQuery();
+
+			// se houver um resultado na busca
 			if (rs.next()) {
-				Department dep = new Department();
-				dep.setId(rs.getInt("DepartmentId"));
-				dep.setName(rs.getString("DepName"));
-				Seller obj = new Seller();
-				obj.setId(rs.getInt("Id"));
-				obj.setName(rs.getString("Name"));
-				obj.setEmail(rs.getString("Email"));
-				obj.setBaseSalary(rs.getDouble("BaseSalary"));
-				obj.setBirthDate(rs.getDate("BirthDate"));
-				obj.setDepartment(dep);
-				
+				// criando o objeto do tipo Department
+				Department dep = instantiateDepartment(rs);
+
+				// criando o objeto do tipo Seller
+				Seller obj = instantiateSeller(rs, dep);
+
+				// retornando o objeto vendedor
 				return obj;
-				
 			}
+
+			// retornando null caso não haja um resultado
 			return null;
-		}
-		catch (SQLException e) {
+		} catch (SQLException e) {
+			// caso ocorra uma exceção, é lançada uma excessão do tipo DbException com a
+			// mensagem de erro
 			throw new DbException(e.getMessage());
-		}
-		finally {
+		} finally {
+			// fechando os recursos abertos pela consulta ao banco de dados
 			DB.closeStatement(st);
 			DB.closeResultSet(rs);
 		}
 	}
 
+	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+		// Criação de um objeto vendedor
+		Seller obj = new Seller();
+
+		// Preenchimento do id do vendedor com o valor obtido do resultado da consulta
+		// ao banco de dados
+		obj.setId(rs.getInt("Id"));
+
+		// Preenchimento do nome do vendedor com o valor obtido do resultado da consulta
+		// ao banco de dados
+		obj.setName(rs.getString("Name"));
+
+		// Preenchimento do email do vendedor com o valor obtido do resultado da
+		// consulta ao banco de dados
+		obj.setEmail(rs.getString("Email"));
+
+		// Preenchimento do salário base do vendedor com o valor obtido do resultado da
+		// consulta ao banco de dados
+		obj.setBaseSalary(rs.getDouble("BaseSalary"));
+
+		// Preenchimento da data de nascimento do vendedor com o valor obtido do
+		// resultado da consulta ao banco de dados
+		obj.setBirthDate(rs.getDate("BirthDate"));
+
+		// Definindo o departamento do vendedor
+		obj.setDepartment(dep);
+
+		// Retornando o objeto vendedor preenchido com os valores obtidos do banco de
+		// dados
+		return obj;
+	}
+
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		// Criando um novo objeto do tipo Department
+		Department dep = new Department();
+
+		// Definindo o id do departamento com o valor obtido na busca
+		dep.setId(rs.getInt("DepartmentId"));
+
+		// Definindo o nome do departamento com o valor obtido na busca
+		dep.setName(rs.getString("DepName"));
+
+		// retornando o objeto departamento
+		return dep;
+	}
+
+	// Método para buscar todos os vendedores
 	@Override
 	public List<Seller> findAll() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 }
