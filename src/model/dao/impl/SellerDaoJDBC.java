af58e6049bc4e5438a7da058a766c71e4c998f82
IvanTenorioDev <tenoriodesenvolvedor@gmail.com>
2023-02-06 13:48:06 -0300

    findByDepartment implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -14,125 +17,112 @@ import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao {
 
+	// Cria a conexão com o banco de dados
 	private Connection conn;
 
-//Constructor que recebe uma conexão como parâmetro e atribui ao atributo da classe
 	public SellerDaoJDBC(Connection conn) {
 		this.conn = conn;
 	}
 
-//Método para inserir vendedor
-	@Override
-	public void insert(Seller obj) {
-		// TODO Auto-generated method stub
-
-	}
-
-//Método para atualizar vendedor
-	@Override
-	public void update(Seller obj) {
-		// TODO Auto-generated method stub
-
-	}
-
-//Método para excluir vendedor por id
-	@Override
-	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
-
-	}
-
-//Método para encontrar vendedor por id
+	// Método que retorna um vendedor de acordo com o id
 	@Override
 	public Seller findById(Integer id) {
-		PreparedStatement st = null;
-		ResultSet rs = null;
+		PreparedStatement st = null; // declara uma variavél st que armazenará o objeto PreparedStatement
+		ResultSet rs = null; // declara uma variável rs que armazena o objeto ResultSet
 		try {
-			// Comando SQL para buscar vendedor pelo id e o departamento associado
+			// cria um objeto PreparedStatement com uma query sql para obter os dados do
+			// vendedor de acordo com o id
 			st = conn.prepareStatement(
 					"SELECT seller.*,department.Name as DepName " + "FROM seller INNER JOIN department "
-							+ "ON seller.DepartmentId = department.Id " + "WHERE seller.Id = ? ");
+							+ "ON seller.DepartmentId = department.Id " + "WHERE seller.Id = ?");
 
-			// definindo o id para busca
+			// define o parâmetro da query sql
 			st.setInt(1, id);
+			// executa a query sql e armazena o resultado no objeto ResultSet
 			rs = st.executeQuery();
-
-			// se houver um resultado na busca
-			if (rs.next()) {
-				// criando o objeto do tipo Department
+			if (rs.next()) { // se conseguiu obter os dados
+				// cria um objeto Department a partir dos dados no ResultSet
 				Department dep = instantiateDepartment(rs);
-
-				// criando o objeto do tipo Seller
+				// cria um objeto Seller a partir dos dados no ResutSet
 				Seller obj = instantiateSeller(rs, dep);
-
-				// retornando o objeto vendedor
-				return obj;
+				return obj; // retorna o objeto Seller criado
 			}
-
-			// retornando null caso não haja um resultado
-			return null;
+			return null; // caso contrário retorna null
 		} catch (SQLException e) {
-			// caso ocorra uma exceção, é lançada uma excessão do tipo DbException com a
-			// mensagem de erro
 			throw new DbException(e.getMessage());
 		} finally {
-			// fechando os recursos abertos pela consulta ao banco de dados
-			DB.closeStatement(st);
-			DB.closeResultSet(rs);
+			DB.closeStatement(st); // fecha o objeto PreparedStatement
+			DB.closeResultSet(rs); // fecha o objeto ResultSet
 		}
 	}
 
-	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
-		// Criação de um objeto vendedor
-		Seller obj = new Seller();
-
-		// Preenchimento do id do vendedor com o valor obtido do resultado da consulta
-		// ao banco de dados
-		obj.setId(rs.getInt("Id"));
-
-		// Preenchimento do nome do vendedor com o valor obtido do resultado da consulta
-		// ao banco de dados
-		obj.setName(rs.getString("Name"));
-
-		// Preenchimento do email do vendedor com o valor obtido do resultado da
-		// consulta ao banco de dados
-		obj.setEmail(rs.getString("Email"));
-
-		// Preenchimento do salário base do vendedor com o valor obtido do resultado da
-		// consulta ao banco de dados
-		obj.setBaseSalary(rs.getDouble("BaseSalary"));
-
-		// Preenchimento da data de nascimento do vendedor com o valor obtido do
-		// resultado da consulta ao banco de dados
-		obj.setBirthDate(rs.getDate("BirthDate"));
-
-		// Definindo o departamento do vendedor
-		obj.setDepartment(dep);
-
-		// Retornando o objeto vendedor preenchido com os valores obtidos do banco de
-		// dados
-		return obj;
-	}
-
+	// cria um objeto Department a partir dos dados no ResultSet
 	private Department instantiateDepartment(ResultSet rs) throws SQLException {
-		// Criando um novo objeto do tipo Department
 		Department dep = new Department();
-
-		// Definindo o id do departamento com o valor obtido na busca
 		dep.setId(rs.getInt("DepartmentId"));
-
-		// Definindo o nome do departamento com o valor obtido na busca
 		dep.setName(rs.getString("DepName"));
-
-		// retornando o objeto departamento
 		return dep;
 	}
 
-	// Método para buscar todos os vendedores
-	@Override
-	public List<Seller> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+	// cria um objeto Seller a partir dos dados no ResutSet
+	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+		Seller obj = new Seller();
+		obj.setId(rs.getInt("Id"));
+		obj.setName(rs.getString("Name"));
+		obj.setEmail(rs.getString("Email"));
+		obj.setBaseSalary(rs.getDouble("BaseSalary"));
+		obj.setBirthDate(rs.getDate("BirthDate"));
+		obj.setDepartment(dep);
+		return obj;
 	}
 
+	// Método que retorna uma lista de vendedores de acordo com o departamento
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			// cria um objeto PreparedStatement com uma query sql para obter os dados dos vendedores de acordo com o departamento
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE DepartmentId = ? "
+					+ "ORDER BY Name");
+			
+			// define o parâmetro da query sql
+			st.setInt(1, department.getId());
+			// executa a query sql e armazena o resultado no objeto ResultSet
+			rs = st.executeQuery();
+			
+			List<Seller> list = new ArrayList<>(); // cria uma lista de Seller
+			Map<Integer, Department> map = new HashMap<>(); // cria um mapa para armazenar os objetos Department
+			
+			// enquanto houver dados no ResultSet
+			while (rs.next()) {
+				
+				Department dep = map.get(rs.getInt("DepartmentId")); // obtém o objeto Department no mapa
+				
+				// se o mapa não tiver um objeto Department para o id obtido
+				if (dep == null) {
+					// cria um objeto Department a partir dos dados no ResultSet
+					dep = instantiateDepartment(rs);
+					// armazena o objeto Department no mapa
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				
+				// cria um objeto Seller a partir dos dados no ResutSet
+				Seller obj = instantiateSeller(rs, dep);
+				list.add(obj); // adiciona o objeto Seller à lista 
+			}
+			return list; // retorna a lista de Seller
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st); // fecha o objeto PreparedStatement
+			DB.closeResultSet(rs); // fecha o objeto ResultSet
+		}
+	}
 }
